01d61c4acfd12b1c67eaf763c833b3b119d482ce
aleafs <zhangxc83@gmail.com>
2012-03-16 21:22:58 +0800

    unittest and code coverage

--- test/urllib.test.js
+++ test/urllib.test.js
@@ -2,114 +2,107 @@
  * Module dependencies.
  */
 
+if (process.env.JSCOV) {
+    var jscover = require('jscoverage');
+    require = jscover.require(module);
+    require(__dirname + '/../', true);
+    process.on('exit', jscover.coverage);
+}
+
 var urllib = require('../');
 var should = require('should');
 
-describe('urllib', function() {
+/* {{{ private function implode_buffer_chunks() */
+function implode_buffer_chunks(chunks)
+{
+    var len = 0;
+    for (var i = 0; i < chunks.length; i++) {
+        len += chunks[i].length;
+    }
 
-	describe('#get_charset()', function() {
-		it('should return the correct charset', function(done) {
-			var cases = [
-		    ['http://baidu.com', null], // <meta http-equiv="refresh" content="0;url=http://www.baidu.com/">
-		    ['http://www.baidu.com', 'gb2312'],
-		    ['http://www.baidu.com/s?kw=cnodejs', 'gbk'],
-		    ['http://club.cnodejs.org/', 'utf-8'],
-		    ['http://cnodejs.org/log', null],
-		    ['http://www.taobao.com', 'gb2312'],
-		    ['http://weibo.com', 'utf-8']
-			];
-			var count = 0;
-			cases.forEach(function(c) {
-				urllib.request(c[0], function(err, data, res) {
-					should.ok(!err);
-					data.should.be.an.instanceof(Buffer);
-					res.statusCode.should.below(400);
-					count++;
-					var end = data.length;
-					if (end > 512) {
-						end = 512;
-					}
-					var log = '\nurl: ' + c[0];
-					log += '\nerror: ' + err;
-					log += '\nstatus: ' + res.statusCode;
-					log += '\nheaders: ' + JSON.stringify(res.headers);
-					log += '\nhtml head: ' + data.slice(0, end).toString();
-					var charset = urllib.get_charset(res, data);
-					// console.log(charset, c[1], c)
-					should.ok(c[1] === charset);
-					//console.log(res.realurl);
-					if (count === cases.length) {
-						done();
-					}
-				});
-			});
-		});
-	});
+    var pos = 0;
+    var buf = new Buffer(len);
+    for (var i = 0; i < chunks.length; i++) {
+        chunks[i].copy(buf, pos);
+        pos += chunks[i].length;
+    }
 
-	describe('#request()', function() {
-		it('should 302', function(done) {
-			urllib.request('http://cnodejs.org', function(err, data, res) {
-				res.should.status(301);
-				done();
-			});
-		});
-	});
+    return buf;
+}
+/* }}} */
+
+/* {{{ mock http server for unittest */
+
+var HTTP    = require('http').createServer(function(req, res) {
+    var chunks  = [];
+    req.on('data', function(buf) {
+        chunks.push(buf);
+    });
+
+    req.on('end', function() {
+        var url = req.url.split('?');
+        var get = require('querystring').parse(url[1]);
+
+        if (chunks.length) {
+            var ret = implode_buffer_chunks(chunks).toString();
+        } else {
+            var ret = '<html><head><meta http-equiv="Content-Type" content="text/html;charset=##{charset}##">...</html>';
+        }
+
+        chunks  = [];
+        res.writeHead(get.code ? get.code : 200, {
+            'Content-Type'  : 'text/html',
+        });
+        res.end(ret.replace('##{charset}##', get.charset ? get.charset : 'i_am_not_defined'));
+
+    });
+}).listen(33749);
+/* }}} */
+
+describe('urllib-test', function() {
+
+    /* {{{ should_mocked_http_service_works_fine() */
+    it('should_mocked_http_service_works_fine' ,function(done) {
+        urllib.request('http://127.0.0.1:33749?a=12&code=302', function(error, data, res) {
+            should.ok(!error);
+            data.should.be.an.instanceof(Buffer);
+            res.statusCode.should.eql(302);
+            done();
+        });
+    });
+    /* }}} */
+
+    /* {{{ should_return_charset_works_fine() */
+    it('should_return_charset_works_fine', function(done) {
+        var charset = ['gb2312', 'gbk', 'utf-8'];
+
+        var waitnum = charset.length + 1;
+        charset.forEach(function(item) {
+            urllib.request('http://127.0.0.1:33749?code=200&charset=' + item, function(error, data, res) {
+                should.ok(!error);
+                item.should.eql(urllib.get_charset(res, data));
+                if ((--waitnum) < 1) {
+                    done();
+                }
+            });
+        });
+
+        var options = {
+            'type'  : 'POST',
+            'data'  : '<html><meta http-equiv="refresh" content="0;url=http://www.baidu.com/"></html>',
+        };
+        urllib.request('http://127.0.0.1:33749', options, function(error, data, res) {
+            if ((--waitnum) < 1) {
+                done();
+            }
+        });
+    
+    });
+    /* }}} */
 
 });
 
-// module.exports = {
-	
-// 	'redirect': function(){
-// 		var cases = [
-//  		    // ['http://baidu.com', 'http://www.baidu.com/'], // <meta http-equiv="refresh" content="0;url=http://www.baidu.com/">
-//  		    ['http://www.baidu.com/s?kw=cnodejs', 'http://www.baidu.com/s?kw=cnodejs'],
-//  		    ['http://cnodejs.org/', 'http://cnodejs.org/blog/'],
-//  		    ['http://taobao.com', 'http://www.taobao.com/'],
-//  		    ['http://t.cn/heVleY', 'http://user.qzone.qq.com/309389312/blog/1305512153']
-//  		];
-// 		cases.forEach(function(c){
-// 			urllib.urlget(c[0], null, {handle_data: false}, function(err, data, res) {
-// 				var log = '\nurl: ' + c[0];
-// 				log += '\nrealurl: ' + res.realurl;
-// 				log += '\nerror: ' + err;
-// 				log += '\nstatus: ' + res.statusCode;
-// 				log += '\nheaders: ' + JSON.stringify(res.headers);
-// 				assert.equal(res.realurl, c[1], log);
-// 			});
-// 		});
-// 	},
-	
-// 	'get_request_options': function() {
-		
-// 	},
-	
-// 	'urlgets': function() {
-// 		var cases = [
-//   		    //['http://baidu.com', 'http://www.baidu.com/'], // <meta http-equiv="refresh" content="0;url=http://www.baidu.com/">
-//   		    ['http://www.baidu.com/s?kw=cnodejs', 'http://www.baidu.com/s?kw=cnodejs'],
-//   		    ['http://cnodejs.org/', 'http://cnodejs.org/blog/'],
-//   		    ['http://taobao.com', 'http://www.taobao.com/'],
-//   		    ['http://t.cn/heVleY', 'http://user.qzone.qq.com/309389312/blog/1305512153']
-//   		];
-// 		var urls = [];
-// 		for(var i = 0; i < cases.length; i++) {
-// 			urls.push(cases[i][0]);
-// 		}
-// 		urllib.urlgets(urls, function(results) {
-// 			for(var i = 0; i < cases.length; i++) {
-// 				var c = cases[i];
-// 				var result = results[c[0]];
-// 				var res = result[2];
-// 				assert.equal(result[0], null);
-// 				assert.ok(result[1].length > 0);
-// 				assert.equal(c[1], res.realurl);
-// 			}
-// 		});
-// 	}
-	
-//	'urlpost': function() {
-//		urllib.urlpost('http://www.postyourtest.com/tests', {kw: 'cnodejs'}, function(err, data, res) {
-//			console.log(res.headers);
-//			console.log(data.slice(0, 512).toString())
-//		});
-//	}
+after(function() {
+    HTTP.close();
+});
+
