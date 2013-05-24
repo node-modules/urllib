64b0543d18c73c3487e733ba488eab43e0c17249
lizhengfu <405574395@qq.com>
2013-05-24 19:03:26 +0800

    add iconv-lite to supported encodings

--- examples/request_cnodejs.js
+++ examples/request_cnodejs.js
@@ -9,9 +9,12 @@
  */
 
 var urllib = require('../');
-
-urllib.request('http://cnodejs.org/', { wd: 'nodejs' }, function (err, data, res) {
+var url = 'http://tieba.baidu.com/f?kw=%E5%8D%8E%E5%8D%97%E7%90%86%E5%B7%A5%E5%A4%A7%E5%AD%A6'
+var args = {
+	decode: 'gbk'
+}
+urllib.request(url, args, function (err, data, res) {
   console.log(res.statusCode);
   console.log(res.headers);
-  console.log(data.toString());
+  console.log(data);
 });
