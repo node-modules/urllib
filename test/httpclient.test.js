6c8cfce55a984f631ca93c59604fc29422a8b9b9
fengmk2 <fengmk2@gmail.com>
2014-09-10 16:34:14 +0800

    httpclient add requestThunk()

---
+++ test/httpclient.test.js
@@ -0,0 +1,30 @@
+/**!
+ * urllib - httpclient.test.js
+ *
+ * Copyright(c) fengmk2 and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var should = require('should');
+var urllib = require('../');
+
+describe('httpclient.test.js', function () {
+  it('should requestThunk()', function (done) {
+    var client = urllib.create();
+    client.requestThunk('http://nodejs.org')(function (err, result) {
+      should.not.exist(err);
+      result.data.should.be.a.Buffer;
+      result.status.should.equal(200);
+      done();
+    });
+  });
+});
