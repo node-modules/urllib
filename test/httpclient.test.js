fb7d39b356bcdfd289338da7ad4136105788eaa0
fengmk2 <m@fengmk2.com>
2015-08-12 21:22:19 +0800

    fix: httpclient support curl too

--- test/httpclient.test.js
+++ test/httpclient.test.js
@@ -17,16 +17,45 @@
 var should = require('should');
 var urllib = require('../');
 
-describe('httpclient.test.js', function () {
+describe('test/httpclient.test.js', function () {
   it('should requestThunk()', function (done) {
     var client = urllib.create();
     client.hasCustomAgent.should.equal(false);
     client.hasCustomHttpsAgent.should.equal(false);
-    client.requestThunk('https://iojs.org')(function (err, result) {
+    client.requestThunk('https://npm.taobao.org', {
+      timeout: 25000
+    })(function (err, result) {
       should.not.exist(err);
       result.data.should.be.a.Buffer;
       result.status.should.equal(200);
       done();
     });
+  });
+
+  it('should curl() with callback', function (done) {
+    var client = urllib.create();
+    client.hasCustomAgent.should.equal(false);
+    client.hasCustomHttpsAgent.should.equal(false);
+    client.curl('https://npm.taobao.org', {
+      timeout: 25000
+    }, function (err, result, res) {
+      should.not.exist(err);
+      result.should.be.a.Buffer;
+      res.status.should.equal(200);
+      done();
+    });
+  });
+
+  it('should curl() with promise', function (done) {
+    var client = urllib.create();
+    client.hasCustomAgent.should.equal(false);
+    client.hasCustomHttpsAgent.should.equal(false);
+    client.curl('https://npm.taobao.org/package/pedding', {
+      timeout: 25000
+    }).then(function (result) {
+      result.data.should.be.a.Buffer;
+      result.status.should.equal(200);
+      done();
+    }).catch(done);
   });
 });
