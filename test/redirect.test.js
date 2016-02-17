b3bd11616bb5e401f1ef4438a6c6062dc02fbb2d
fengmk2 <m@fengmk2.com>
2016-02-17 10:53:23 +0800

    fix: keep headers.Host on `location: /foo` redirect

--- test/redirect.test.js
+++ test/redirect.test.js
@@ -13,20 +13,47 @@
  */
 
 var dns = require('dns');
+var config = require('./config');
 var urllib = require('../');
 
 describe('test/redirect.test.js', function() {
-  it('should work with headers.Host', function(done) {
-    // https://registry.npm.taobao.org/pedding/download/pedding-1.0.0.tgz
-    dns.lookup('registry.npm.taobao.org', function(err, address) {
+  it('should redirect `location: /package/pedding` with headers.Host', function(done) {
+    var domain = config.npmWeb.replace('https://', '');
+    dns.lookup(domain, function(err, address) {
       if (err) {
         return done(err);
       }
-      var url = 'https://' + address + '/pedding/download/pedding-1.0.0.tgz';
+      var url = 'http://' + address + '/pedding';
       urllib.request(url, {
         timeout: 30000,
         headers: {
-          Host: 'registry.npm.taobao.org',
+          Host: domain,
+        },
+        followRedirect: true,
+      }, function(err, data, res) {
+        if (err) {
+          return done(err);
+        }
+        console.log(res.statusCode, res.headers);
+        res.statusCode.should.equal(200);
+        data.length.should.above(100);
+        done();
+      });
+    });
+  });
+
+  it('should redirect `location: http://other-domain` with headers.Host', function(done) {
+    // https://r.cnpmjs.org/pedding/download/pedding-1.0.0.tgz
+    var domain = 'r.cnpmjs.org';
+    dns.lookup(domain, function(err, address) {
+      if (err) {
+        return done(err);
+      }
+      var url = 'http://' + address + '/pedding';
+      urllib.request(url, {
+        timeout: 30000,
+        headers: {
+          Host: domain,
         },
         followRedirect: true,
       }, function(err, data, res) {
