76f219e78b35d58ba3b486e827e258e3859ea3f5
fengmk2 <fengmk2@gmail.com>
2021-11-24 13:31:12 +0800

    test: add tsd for timing interface
    https://github.com/node-modules/urllib/pull/375

--- lib/index.test-d.ts
+++ lib/index.test-d.ts
@@ -8,3 +8,12 @@ expectType<Buffer>((await curl<Buffer>('http://a.com', {})).data);
 expectType<string>((await curl<string>('http://a.com', {
   method: 'HEAD',
 })).data);
+
+// HttpClientResponse
+const res = await curl<Buffer>('http://a.com');
+expectType<number | undefined>(res.res.timing?.queuing);
+expectType<number | undefined>(res.res.timing?.dnslookup);
+expectType<number | undefined>(res.res.timing?.connected);
+expectType<number | undefined>(res.res.timing?.requestSent);
+expectType<number | undefined>(res.res.timing?.waiting);
+expectType<number | undefined>(res.res.timing?.contentDownload);
