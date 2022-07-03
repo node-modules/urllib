cc1c854f45bc5e48716ffca650d1e58385fe24eb
fengmk2 <fengmk2@gmail.com>
2022-07-03 17:59:48 +0800

    📦 NEW: Support options.opaque = object

---
+++ test/options.opaque.test.ts
@@ -0,0 +1,48 @@
+import { strict as assert } from 'assert';
+import urllib from '../src';
+import { startServer } from './fixtures/server';
+
+describe('options.opaque.test.ts', () => {
+  let close: any;
+  let _url: string;
+  beforeAll(async () => {
+    const { closeServer, url } = await startServer();
+    close = closeServer;
+    _url = url;
+  });
+
+  afterAll(async () => {
+    await close();
+  });
+
+  it('should opaque work', async () => {
+    const response = await urllib.request(_url, {
+      dataType: 'json',
+      opaque: {
+        traceId: 'some random id here',
+      },
+    });
+    assert.equal(response.status, 200);
+    assert.deepEqual(response.opaque, {
+      traceId: 'some random id here',
+    });
+  });
+
+  it('should opaque work on error request', async () => {
+    await assert.rejects(async () => {
+      await urllib.request(`${_url}socket.end.error`, {
+        opaque: {
+          traceId: 'some random id here',
+        },
+      });
+    }, (err: any) => {
+      // console.error(err);
+      assert.equal(err.res.status, 200);
+      assert.equal(err.name, 'HTTPParserError');
+      assert.deepEqual(err.opaque, {
+        traceId: 'some random id here',
+      });
+      return true;
+    });
+  });
+});
