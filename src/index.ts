d101f13956709dec0442220e1dec70990cd9b127
fengmk2 <fengmk2@gmail.com>
2022-07-17 16:11:19 +0800

    🐛 FIX: export getGlobalDispatcher (#387)

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export async function request(url: RequestURL, options?: RequestOptions) {
 }
 
 export { HttpClient, HEADER_USER_AGENT as USER_AGENT } from './HttpClient';
-export { MockAgent, setGlobalDispatcher } from 'undici';
+export { MockAgent, setGlobalDispatcher, getGlobalDispatcher } from 'undici';
 
 export default {
   request,
