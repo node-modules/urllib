01d1161681bca59c70666e138d91a486d1f27db8
fengmk2 <fengmk2@gmail.com>
2022-07-26 01:22:31 +0800

    f

--- test/options.digestAuth.test.ts
+++ test/options.digestAuth.test.ts
@@ -47,50 +47,30 @@ describe('options.digestAuth.test.ts', () => {
     });
   });
 
-  it.skip('should request with digest auth success in webdav', async () => {
-    var url = 'http://test.webdav.org/auth-digest/user3';
-    const response = await urllib.request(url, {
-      digestAuth: 'user3:user3',
-      timeout: 20000,
-      dataType: 'text',
-    });
-    assert.equal(response.status, 404);
-    assert.match(response.data, /<p>The requested URL \/auth-digest\/user3 was not found on this server.<\/p>/);
-  });
-
-  it.skip('should request with digest auth fail in webdav', async () => {
-    var url = 'http://test.webdav.org/auth-digest/user4';
-    const response = await urllib.request(url, {
-      digestAuth: 'user3:fail',
-      timeout: 20000,
-      dataType: 'text',
-    });
-    assert.equal(response.status, 401);
-    assert(response.headers['www-authenticate']);
-    assert.match(response.data, /401 Authorization Required/);
-  });
-
   it.skip('should request with digest auth success in httpbin', async () => {
-    var url = 'http://httpbin.org/digest-auth/auth/user/passwd';
+    var url = 'https://httpbin.org/digest-auth/auth/user/passwd';
     const response = await urllib.request(url, {
       digestAuth: 'user:passwd',
       dataType: 'json',
       timeout: 10000,
     });
+    console.log(response.headers);
     assert.equal(response.status, 200);
     assert.deepEqual(response.data, {
-      user: "user",
+      user: 'user',
       authenticated: true,
     });
   });
 
   it.skip('should request with digest auth fail in httpbin', async () => {
-    var url = 'http://httpbin.org/digest-auth/auth/user/passwd';
+    var url = 'https://httpbin.org/digest-auth/auth/user/passwd';
     const response = await urllib.request(url, {
       digestAuth: 'user:passwdfail',
       dataType: 'json',
       timeout: 10000,
     });
+    // console.log(response.headers);
+    assert(response.headers['www-authenticate']);
     assert.equal(response.status, 401);
     assert.equal(response.data, null);
   });
