fa998e2c191f315c7f314c02537290ab9d1e593a
gemwuu <gemwuu@163.com>
2022-10-31 10:10:12 +0800

    test: skip windows socket test

--- test/options.socketPath.test.ts
+++ test/options.socketPath.test.ts
@@ -2,19 +2,18 @@ import { describe, it, beforeAll, afterAll } from 'vitest';
 import { strict as assert } from 'assert';
 import urllib from '../src';
 import { startServer } from './fixtures/socket_server';
-import { createServer } from 'http';
 import os from 'os';
 
 
 describe('options.socketPath.test.ts', () => {
-  if (os.platform() === 'win32') {
-    return;
-  }
-
   let close: any;
   let _url: string;
   let _socketPath: string;
   beforeAll(async () => {
+    if (os.platform() === 'win32') {
+      return;
+    }
+
     const { url, closeServer, socketPath } = await startServer();
     close = closeServer;
     _url = url;
@@ -23,10 +22,14 @@ describe('options.socketPath.test.ts', () => {
   });
 
   afterAll(async () => {
-    await close();
+    await close?.();
   });
 
   it('should request socket successfully', async () => {
+    if (os.platform() === 'win32') {
+      return;
+    }
+
     const result = await urllib.request(_url, {
       socketPath: _socketPath,
       contentType: 'json',
