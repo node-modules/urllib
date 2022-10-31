5c0b4e99ffc8b3ddf9ee40d1cd580f02a80bc712
Ke Wu <gemwuu@163.com>
2022-10-31 23:11:00 +0800

    test: use vitest skipIf api (#419)

--- test/options.socketPath.test.ts
+++ test/options.socketPath.test.ts
@@ -5,15 +5,11 @@ import { startServer } from './fixtures/socket_server';
 import os from 'os';
 
 
-describe('options.socketPath.test.ts', () => {
+describe.skipIf(os.platform() === 'win32')('options.socketPath.test.ts', () => {
   let close: any;
   let _url: string;
   let _socketPath: string;
   beforeAll(async () => {
-    if (os.platform() === 'win32') {
-      return;
-    }
-
     const { url, closeServer, socketPath } = await startServer();
     close = closeServer;
     _url = url;
@@ -26,10 +22,6 @@ describe('options.socketPath.test.ts', () => {
   });
 
   it('should request socket successfully', async () => {
-    if (os.platform() === 'win32') {
-      return;
-    }
-
     const result = await urllib.request(_url, {
       socketPath: _socketPath,
       contentType: 'json',
@@ -39,3 +31,4 @@ describe('options.socketPath.test.ts', () => {
     assert.deepStrictEqual(result.data, {a: 1});
   });
 });
+
