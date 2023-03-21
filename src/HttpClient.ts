0120f6bb89c964d7897cf58b5e2594dab5b04f53
fengmk2 <fengmk2@gmail.com>
2023-03-21 22:10:21 +0800

    f

--- src/HttpClient.ts
+++ src/HttpClient.ts
@@ -315,8 +315,10 @@ export class HttpClient extends EventEmitter {
         bodyTimeout,
         opaque: internalOpaque,
         dispatcher: args.dispatcher ?? this.#dispatcher,
-        reset: typeof options?.reset === 'boolean' ? options.reset : undefined,
       };
+      if (typeof args.reset === 'boolean') {
+        requestOptions.reset = args.reset;
+      }
 
       if (args.followRedirect === false) {
         requestOptions.maxRedirections = 0;
