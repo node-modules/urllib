358321962bf80e0859c2cc8fa3d35fa2dd6b2e98
fengmk2 <fengmk2@gmail.com>
2023-05-24 11:03:48 +0800

    fix: support request uds and tcp at the same time (#451)
    https://github.com/cnpm/rapid/pull/10

--- test/options.socketPath.test.ts
+++ test/options.socketPath.test.ts
@@ -8,26 +8,66 @@ describe.skipIf(os.platform() === 'win32')('options.socketPath.test.ts', () => {
   let close: any;
   let _url: string;
   let _socketPath: string;
+  let server2: any;
   beforeAll(async () => {
     const { url, closeServer, socketPath } = await startServer();
     close = closeServer;
     _url = url;
     _socketPath = socketPath;
-
+    server2 = await startServer();
   });
 
   afterAll(async () => {
-    await close?.();
+    await close();
+    await server2.closeServer();
   });
 
   it('should request socket successfully', async () => {
-    const result = await urllib.request(_url, {
+    let result = await urllib.request(_url, {
       socketPath: _socketPath,
       contentType: 'json',
       dataType: 'json',
     });
 
     assert.deepStrictEqual(result.data, { a: 1 });
+    result = await urllib.request(_url, {
+      socketPath: _socketPath,
+      contentType: 'json',
+      dataType: 'json',
+    });
+    assert.deepStrictEqual(result.data, { a: 1 });
+    result = await urllib.request(_url, {
+      socketPath: _socketPath,
+      contentType: 'json',
+      dataType: 'json',
+    });
+    assert.deepStrictEqual(result.data, { a: 1 });
+    assert(result.res.socket.handledResponses > 1);
+
+    result = await urllib.request('http://unix/api/v1', {
+      socketPath: server2.socketPath,
+      contentType: 'json',
+      dataType: 'json',
+    });
+    assert.deepStrictEqual(result.data, { a: 1 });
+    assert.equal(result.url, 'http://unix/api/v1');
+
+    result = await urllib.request(_url, {
+      socketPath: _socketPath,
+      contentType: 'json',
+      dataType: 'json',
+    });
+    assert.deepStrictEqual(result.data, { a: 1 });
+    assert.equal(result.url, _url);
+
+    // request normal tcp should work
+    const host = process.env.CI ? 'registry.npmjs.org' : 'registry.npmmirror.com';
+    const url = `${host}/urllib/latest`;
+    const result2 = await urllib.request(url, {
+      dataType: 'json',
+    });
+    assert.equal(result2.status, 200);
+    assert.equal(result2.data.name, 'urllib');
   });
 });
 
