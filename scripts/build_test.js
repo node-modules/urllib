a0c7675bd19d26e5895eca39b828805239ec8c39
fengmk2 <fengmk2@gmail.com>
2023-09-14 13:20:17 +0800

    f

--- scripts/build_test.js
+++ scripts/build_test.js
@@ -1,13 +1,14 @@
-import { spawnSync } from 'child_process';
+import { execSync } from 'child_process';
 
-async function main() {
+function main() {
   if (process.version.startsWith('v14.')) {
     console.log(`ignore build:test on Node.js ${process.version}`);
     return;
   }
   const cwd = process.cwd()
-  spawnSync('npm run build:test', {
+  execSync('npm run build:test', {
     cwd,
+    stdio: [ 'inherit', 'inherit', 'inherit' ],
   });
 }
 
