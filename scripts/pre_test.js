5bb3a62ef78305a6552a622d6ddf2c1e82276965
fengmk2 <fengmk2@gmail.com>
2023-12-05 16:03:02 +0800

    test: use vitest v1 (#478)

--- scripts/pre_test.js
+++ scripts/pre_test.js
@@ -4,9 +4,9 @@ function main() {
   if (!process.version.startsWith('v14.')) {
     return;
   }
-  console.log(`use vitest@^0.32.0 on Node.js ${process.version}`);
+  console.log(`use vitest@^0.33.0 on Node.js ${process.version}`);
   const cwd = process.cwd()
-  execSync('npm i vitest@^0.32.0 @vitest/coverage-v8@^0.32.0', {
+  execSync('npm i vitest@^0.33.0 @vitest/coverage-v8@^0.33.0', {
     cwd,
     stdio: [ 'inherit', 'inherit', 'inherit' ],
   });
