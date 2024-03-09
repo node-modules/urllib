0a7682c2b6496609b5f2b84a989c0ec7e9bad91c
fengmk2 <suqian.yf@antgroup.com>
2024-03-09 18:49:10 +0800

    f

--- test/options.signal.test.ts
+++ test/options.signal.test.ts
@@ -1,4 +1,5 @@
 import { strict as assert } from 'node:assert';
+import { EventEmitter } from 'node:events';
 import { describe, it, beforeAll, afterAll } from 'vitest';
 import urllib from '../src';
 import { startServer } from './fixtures/server';
@@ -25,6 +26,24 @@ describe('options.signal.test.ts', () => {
       });
       await sleep(100);
       abortController.abort();
+      await p;
+    }, (err: any) => {
+      // console.error(err);
+      assert.equal(err.name, 'AbortError');
+      assert.equal(err.message, 'Request aborted');
+      assert.equal(err.code, 'UND_ERR_ABORTED');
+      return true;
+    });
+  });
+
+  it('should throw error when EventEmitter emit abort event', async () => {
+    await assert.rejects(async () => {
+      const ee = new EventEmitter();
+      const p = urllib.request(`${_url}?timeout=2000`, {
+        signal: ee,
+      });
+      await sleep(100);
+      ee.emit('abort');
       await p;
     }, (err: any) => {
       // console.error(err);
