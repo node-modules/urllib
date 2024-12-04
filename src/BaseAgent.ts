457fa295900ec512aaa086b8f6620fad29178566
killagu <zhubin.gzb@antgroup.com>
2024-12-04 22:52:17 +0800

    f

--- src/BaseAgent.ts
+++ src/BaseAgent.ts
@@ -6,11 +6,11 @@ import { AsyncLocalStorage } from 'node:async_hooks';
 import { FetchOpaque } from './FetchOpaqueInterceptor.js';
 
 export interface BaseAgentOptions extends Agent.Options {
-  opaqueLocalStorage: AsyncLocalStorage<FetchOpaque>;
+  opaqueLocalStorage?: AsyncLocalStorage<FetchOpaque>;
 }
 
 export class BaseAgent extends Agent {
-  #opaqueLocalStorage: AsyncLocalStorage<FetchOpaque>;
+  #opaqueLocalStorage?: AsyncLocalStorage<FetchOpaque>;
 
   constructor(options: BaseAgentOptions) {
     super(options);
@@ -18,8 +18,10 @@ export class BaseAgent extends Agent {
   }
 
   dispatch(options: Agent.DispatchOptions, handler: Dispatcher.DispatchHandler): boolean {
-    const opaque = this.#opaqueLocalStorage.getStore();
-    (handler as any).opaque = opaque;
+    const opaque = this.#opaqueLocalStorage?.getStore();
+    if (opaque) {
+      (handler as any).opaque = opaque;
+    }
     return super.dispatch(options, handler);
   }
 }
