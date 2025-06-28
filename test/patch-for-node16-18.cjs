333b129e3c3af4ed9bebe2c48a619c07d1357820
fengmk2 <fengmk2@gmail.com>
2025-06-28 22:01:22 +0800

    f

--- test/patch-for-node16-18.cjs
+++ test/patch-for-node16-18.cjs
@@ -11,3 +11,8 @@ const crypto = require('node:crypto');
 if (typeof crypto.getRandomValues !== 'function') {
   crypto.getRandomValues = crypto.webcrypto.getRandomValues.bind(crypto.webcrypto);
 }
+
+// undici@v7 require String.prototype.toWellFormed, patch on Node.js 16 and 18
+if (typeof String.prototype.toWellFormed !== 'function') {
+  require('string.prototype.towellformed/auto');
+}
